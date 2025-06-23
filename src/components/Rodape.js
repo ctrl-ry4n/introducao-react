3ca257d5320acba2e937c73e10a705121ab53328
Ryan <ryryck67@gmail.com!>
2025-06-23 16:30:02 -0300

    atualizacao de exercicios

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from '../styles/Rodape.module.css';
 
-function Topo() {
+function Rodape() {
     return(
         <footer className={styles.rodape}>
             <h1>Desenvolvido com DevMedia</h1>
         </footer>
     )
 }
-export default Topo;
+export default Rodape;
--- src/components/Topo.js
+++ src/components/Topo.js
@@ -5,23 +5,33 @@ import styles from '../styles/Topo.module.css';
 import Menu from './ex5/Menu'
 import { VscMenu } from "react-icons/vsc";
 import { AiOutlineClose } from "react-icons/ai";
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
+
+
 
 function Topo() {
 
     const [menuAberto, setMenuAberto] = useState(false);
+    const [alterarTema, setAlterarTema] = useState(false);
 
     function atualizarBotao(){
         setMenuAberto(!menuAberto);
     };
 
+    function trocarTemaPagina(){
+        setAlterarTema(!alterarTema)
+    };
+
     return(
-    <>
-        <header className={styles.topo}>
+    <div className={alterarTema ? styles.light_mode : styles.dark_mode} >
+        <header>
             <button className={styles.botaoMenu} onClick={atualizarBotao}>{menuAberto ? <AiOutlineClose/> : <VscMenu/>}</button>
             <h1>Introdução a React</h1>
+            <button className={styles.alteraTema} onClick={trocarTemaPagina}>{alterarTema ? <MdOutlineDarkMode/> : <MdOutlineLightMode/>}</button>
         </header>
         {menuAberto && (<Menu/>)}
-    </>
+    </div>
     )
 }
 export default Topo;
